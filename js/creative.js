d80e14f4f0ef7cae34183ba44ca91743b7c36eb0
the101tech <44884653+the101tech@users.noreply.github.com>
2019-01-19 18:16:29 -0800

    Many Changes
    reverted original padding fixes
    
    testing with navbar 100px & logo height 100px - seems to be working. May want logo bigger though
    
    -updated padding in CSS & for portfolio include.
    
    created portfolio class to remove extra white space
    
    fixed scrollspy offset

--- js/creative.js
+++ js/creative.js
@@ -19,7 +19,7 @@
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: 51
+        offset: 100
     })
 
     // Closes the Responsive Menu on Menu Item Click
